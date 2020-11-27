2c40c0ddef45bf18074220f09cd91532a4159498
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-11-27 08:32:49 -0500

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 function App() {
-  function inputchanged() {}
+  function inputChanged() {
+    console.log("added");
+  }
   return (
     <div className="container">
       <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <input
+        onChange={inputChanged}
+        type="text"
+        placeholder="What's your name?"
+      />
       <button>Submit</button>
     </div>
   );
